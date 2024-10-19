c7e61199d6de96ac4cc221d37b62130ce586efd5
Julia <koitejrk@gmail.com>
2024-10-19 17:12:36 -0500

    function

--- parse.py
+++ parse.py
@@ -2,17 +2,18 @@ import requests
 
 
 API_Key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjRiZGE5NmItZjUzNS00MjBjLThjZDQtYjllYjYyNTE2OGQwIiwidHlwZSI6ImFwaV90b2tlbiJ9.JzTeIYcTmrEHgUdWrLHWIxnBaQRBNqfIr-TNxTRkVEw'
+
+def parse_file(file_name):
 #make file path user's input file 
-file_path = 'hospital bill format_2_.pdf'
+    file = "uplaods/{file_name}"
+
+    with open(file, 'rb') as f:
+        response = requests.post(
+            'https://api.edenai.run/v1/pretrained/financial_document_parsing',
+            headers={'Authorization': f'Bearer {API_Key}'},
+            files={'file': f}
+        )
 
 
-with open(file_path, 'rb') as f:
-    response = requests.post(
-        'https://api.edenai.run/v1/pretrained/financial_document_parsing',
-        headers={'Authorization': f'Bearer {API_Key}'},
-        files={'file': f}
-    )
-
-
-data = response.json()
-print(data)
+    data = response.json()
+    print(data)
