7765ab2957b13ca717c5d8e0d5167acc21c85932
Maya <mayavajit@gmail.com>
2024-10-20 09:44:44 -0500

    added streamlit table and detecting duplicates - also added files for testing out different llms

--- parse.py
+++ parse.py
@@ -34,9 +34,20 @@ def parse_file(file_name):
             response_str = response.text
             json_data = json.loads(response_str)
             print(json_data)
-            items_arr = []
-            
-
+            items_dict = dict()
+            item_lines = json_data['amazon']['extracted_data'][0]['item_lines']
+            for item in item_lines:
+                item_desc = item['description'] 
+                if item_desc in items_dict.keys():
+                    print("DUPLICATE:" + item_desc)
+                    items_dict[item_desc] = (item['amount_line'], items_dict[item_desc][1] + 1) #if duplicate (double charge)
+                else:
+                    items_dict[item_desc] = (item['amount_line'], 1)
+            for key in items_dict.keys():
+                print("Item: " + key)
+            for value in items_dict.values():
+                print("Amount" + str(value))
+            return items_dict
 
         else:
             print(f"Error: {response.status_code} - {response.text}")
